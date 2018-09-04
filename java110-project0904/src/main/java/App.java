a5f4aa1139840a71699380539f57c20b514d0c03
choi <sjchoi0408@hanmail.net>
2018-09-04 19:57:25 +0900

    메서드 활용 리펙토링

--- java110-project0904/src/main/java/App.java
+++ java110-project0904/src/main/java/App.java
@@ -1,38 +1,53 @@
 import java.util.Scanner;
 
 public class App {
+
+    static String[] name = new String[100];
+    static String[] address = new String[100];
+    static String[] id = new String[100];
+    
+    static Scanner sc = new Scanner(System.in);
+    static int index = 0;
+
     public static void main(String[] args) {
       
-        String[] name = new String[100];
-        String[] address = new String[100];
-        String[] id = new String[100];
+        Input();
+        Output();
+        sc.close();
         
-        int index = 0;
-        
-        Scanner sc = new Scanner(System.in);
-        
+    }
+    
+    static void Input()
+    {
         while(true)
         {
-        System.out.println("이름: ");
-        name[index] = sc.nextLine();
-        
-        System.out.println("주소: ");
-        address[index] = sc.nextLine();
-        
-        System.out.println("학번: ");
-        id[index] = sc.nextLine();
-        
-        index++;
-        
-        
-        System.out.println("계속 입력하시겠습니까? (y/n)");
-        String yn = sc.nextLine();
-        
-        if(yn.toLowerCase().equals("n"))
-            break;
-        
+            System.out.println("이름: ");
+            name[index] = sc.nextLine();
+            
+            System.out.println("주소: ");
+            address[index] = sc.nextLine();
+            
+            System.out.println("학번: ");
+            id[index] = sc.nextLine();
+            
+            index++;
+            
+            
+            System.out.println("계속 입력하시겠습니까? (y/n)");
+            String yn = sc.nextLine();
+            
+            if(yn.toLowerCase().equals("n"))
+                break;
+            
         }
-        for(int i=0; i<index; i++)
-        System.out.printf("%s, %s, %s\n", name[i], address[i], id[i]);
+        
     }
+    static void Output()
+    {
+        for(int i=0; i<index; i++)
+            System.out.printf("%s, %s, %s\n", name[i], address[i], id[i]);
+        
+    }
+    
+    
 }
