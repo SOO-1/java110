a5e4eba9d167a87b56b70e995d2b38580f38c8c2
choi <sjchoi0408@hanmail.net>
2018-09-04 15:42:58 +0900

    클래스 활용

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,10 +2,40 @@ import java.util.Scanner;
 
 public class App {
     
-    static String[] names = new String[100];        //클래스변수
+    //여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
+    static class Member{
+        protected String name;      //field(변수지칭)
+        protected String email;
+        protected String password;
+        
+        //getName() =getter, setName() =setter       =>operator/accessor/property/message
+        //인스턴스의 메모리를 다루는 연산자(operator=setter&getter=accessor=property=message)
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        
+    }
+    
+/*    static String[] names = new String[100];        //클래스변수
     static String[] emails = new String[100];
     static String[] passwords = new String [100];
-    
+*/    
+    static Member[] members = new Member[100];
     static int index = 0;
     
     // 1)키보드 입력을 처리할 객체 준비
@@ -24,23 +54,29 @@ public class App {
         
     static void printMembers() {
         for(int i=0; i<index; i++)
-        System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);        
+        System.out.printf("%s, %s, %s\n", members[i].getName(), members[i].getEmail(), members[i].getPassword());        
     }
 
     static void inputMembers() {
 
         while(true)
         {
+//            members[index] = new Member();
+            Member m = new Member();
+            
             System.out.print("이름? ");
-            names[index] = keyIn.nextLine();   //사용자가 한줄을 입력햇을 때 비로소 nextLine은 string을 return한다.
+            m.setName(keyIn.nextLine());
+//            memgers[index].setName(keyIn.nextLine());
+//            names[index] = keyIn.nextLine();   //사용자가 한줄을 입력햇을 때 비로소 nextLine은 string을 return한다.
                                             //호출하자마자 return하는것 X
             System.out.print("이메일? ");
-            emails[index] = keyIn.nextLine();
+            m.setEmail(keyIn.nextLine());
         
             System.out.print("암호? ");
-            passwords[index] = keyIn.nextLine();
+            m.setPassword(keyIn.nextLine());
             
-            index++;
+            members[index++] = m;   //index에 넣고 , index값을 이후에 증가시킴.
+//            index++;
             
             System.out.println("계속 하시겠습니까? (Y/n)"); //둘중 하나가 대문자라면 그 값이 기본값!(enter만해도 Y로 인식)
             String answer = keyIn.nextLine();
