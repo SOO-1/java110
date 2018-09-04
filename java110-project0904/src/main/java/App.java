96834845060b2915e9b999a060520be976e2174a
choi <sjchoi0408@hanmail.net>
2018-09-04 19:50:15 +0900

    배열 활용

--- java110-project0904/src/main/java/App.java
+++ java110-project0904/src/main/java/App.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
       
+        String[] name = new String[100];
+        String[] address = new String[100];
+        String[] id = new String[100];
+        
+        int index = 0;
+        
         Scanner sc = new Scanner(System.in);
         
         while(true)
         {
         System.out.println("이름: ");
-        String name = sc.nextLine();
+        name[index] = sc.nextLine();
         
         System.out.println("주소: ");
-        String address = sc.nextLine();
+        address[index] = sc.nextLine();
         
         System.out.println("학번: ");
-        String id = sc.nextLine();
+        id[index] = sc.nextLine();
         
-        System.out.printf("%s, %s, %s\n", name, address, id);
+        index++;
+        
         
         System.out.println("계속 입력하시겠습니까? (y/n)");
         String yn = sc.nextLine();
@@ -25,6 +32,7 @@ public class App {
             break;
         
         }
-        
+        for(int i=0; i<index; i++)
+        System.out.printf("%s, %s, %s\n", name[i], address[i], id[i]);
     }
 }
