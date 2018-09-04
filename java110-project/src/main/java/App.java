06644ec78f5f4e3796526841f59732ae374f5e0f
choi <sjchoi0408@hanmail.net>
2018-09-04 12:45:35 +0900

    반복문 이용하여 반복 입력 기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -6,15 +6,27 @@ public class App {
         Scanner keyIn = new Scanner(System.in);
         
         // 2) 사용자로부터 회원 정보 입력받기
-        System.out.print("이름? ");
-        String name = keyIn.nextLine();   //사용자가 한줄을 입력햇을 때 비로소 nextLine은 string을 return한다.
+        
+        while(true)
+        {
+            System.out.print("이름? ");
+            String name = keyIn.nextLine();   //사용자가 한줄을 입력햇을 때 비로소 nextLine은 string을 return한다.
                                             //호출하자마자 return하는것 X
-        System.out.print("이메일? ");
-        String email = keyIn.nextLine();
+            System.out.print("이메일? ");
+            String email = keyIn.nextLine();
         
-        System.out.print("암호? ");
-        String password = keyIn.nextLine();
+            System.out.print("암호? ");
+            String password = keyIn.nextLine();
         
-        System.out.printf("%s, %s, %s\n", name, email, password);
+        
+            System.out.printf("%s, %s, %s\n", name, email, password);
+            
+            System.out.println("계속 하시겠습니까? (Y/n)"); //둘중 하나가 대문자라면 그 값이 기본값!(enter만해도 Y로 인식)
+            String answer = keyIn.nextLine();
+            
+            if(answer.toLowerCase().equals("n"))    //return값에 대해 equals메서드 호출
+                break;
+            
+        }
     }
 }
