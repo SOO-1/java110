cd120aaee0d1f34536c0aeead44a0e4a0747989e
choi <sjchoi0408@hanmail.net>
2018-09-05 11:02:12 +0900

    강사관리 기능추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -58,13 +58,43 @@ public class App {
         
     }
     
+    static class Teacher extends Member{
+        protected String tel;
+        protected int pay;
+        protected String subjects;
+        
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+        public int getPay() {
+            return pay;
+        }
+        public void setPay(int pay) {
+            this.pay = pay;
+        }
+        public String getSubjects() {
+            return subjects;
+        }
+        public void setSubjects(String subjects) {
+            this.subjects = subjects;
+        }
+        
+        
+    }
+    
 /*    static String[] names = new String[100];        //클래스변수
     static String[] emails = new String[100];
     static String[] passwords = new String [100];
 */    
 //    static Student[] students = new Student[100];
     static Student[] students = new Student[100];
-    static int index = 0;
+    static Teacher[] teachers = new Teacher[100];
+    
+    static int studentIndex = 0;
+    static int teacherIndex = 0;
     
     // 1)키보드 입력을 처리할 객체 준비
     static Scanner keyIn = new Scanner(System.in); //scanner는 쓴 후에 닫아야 함.
@@ -80,9 +110,10 @@ public class App {
           
             serviceStudentMenu();
             
+        }else if(menu.equals("2")){
+            serviceTeacherMenu();
         }else if(menu.equals("0")){
             System.out.println("안녕히가세요!");
-        }else {
             break;
         }
      }
@@ -109,6 +140,26 @@ public class App {
          }
     }
 
+    private static void serviceTeacherMenu() {
+        while(true) {
+            System.out.println("강사 관리>");
+            String command = keyIn.nextLine();              
+        
+        if(command.equals("list")) {
+            printTeachers();
+        }else if(command.equals("add")) {
+            inputTeachers();
+        }else if(command.equals("quit")) {
+            break;
+        }else {
+            System.out.println("유효하지 않은 명령입니다.");
+        }
+        
+        
+         }
+    }
+    
+    
     private static String promptMenu() {
         System.out.println("[메뉴]");
         System.out.println("1. 학생 관리");
@@ -147,9 +198,9 @@ public class App {
     static void printStudents() {
         int count = 0;
         for(Student s : students) {
-            if(count++ == index)
+            if(count++ == studentIndex)
                 break;
-        System.out.printf("%s, %s, %s, %s, %s, %s\n", 
+        System.out.printf("%s, %s, %s, %s, %b, %s\n", 
                 s.getName(),
                 s.getEmail(), 
                 s.getPassword(),
@@ -158,6 +209,23 @@ public class App {
                 s.getTel()); 
         }
     }
+    
+    
+    static void printTeachers() {
+        int count = 0;
+        for(Teacher s : teachers) {
+            if(count++ == teacherIndex)
+                break;
+        System.out.printf("%s, %s, %s, %s, %d, [%s]\n", 
+                s.getName(),
+                s.getEmail(), 
+                s.getPassword(),
+                s.getTel(),
+                s.getPay(),
+                s.getSubjects()); 
+        }
+    }
+    
 
     static void inputStudents() {
 
@@ -187,7 +255,7 @@ public class App {
             m.setTel(keyIn.nextLine());
             
             
-            students[index++] = m;   //index에 넣고 , index값을 이후에 증가시킴.
+            students[studentIndex++] = m;   //index에 넣고 , index값을 이후에 증가시킴.
 //            index++;
             
             System.out.println("계속 하시겠습니까? (Y/n)"); //둘중 하나가 대문자라면 그 값이 기본값!(enter만해도 Y로 인식)
@@ -197,5 +265,49 @@ public class App {
         }
         
     }
+
+    
+    static void inputTeachers() {
+
+        while(true)
+        {
+//            members[index] = new Member();
+            Teacher m = new Teacher();
+            
+            System.out.print("이름? ");
+            m.setName(keyIn.nextLine());
+//            memgers[index].setName(keyIn.nextLine());
+//            names[index] = keyIn.nextLine();   //사용자가 한줄을 입력햇을 때 비로소 nextLine은 string을 return한다.
+                                            //호출하자마자 return하는것 X
+            System.out.print("이메일? ");
+            m.setEmail(keyIn.nextLine());
+        
+            System.out.print("암호? ");
+            m.setPassword(keyIn.nextLine());
+
+            System.out.print("전화? ");
+            m.setTel(keyIn.nextLine());
+
+            System.out.print("시급? ");
+            m.setPay(Integer.parseInt(keyIn.nextLine()));
+
+            System.out.print("강의과목? (예 : 자바, C, C++) ");
+            m.setSubjects(keyIn.nextLine());
+            
+            
+            teachers[teacherIndex++] = m;   //index에 넣고 , index값을 이후에 증가시킴.
+//            index++;
+            
+            System.out.println("계속 하시겠습니까? (Y/n)"); //둘중 하나가 대문자라면 그 값이 기본값!(enter만해도 Y로 인식)
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n"))    //return값에 대해 equals메서드 호출
+                break;
+        }
+        
+    }
+    
+    
+    
+    
     
 }
