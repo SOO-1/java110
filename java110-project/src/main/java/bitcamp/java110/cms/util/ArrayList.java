9367c0e11823e7d2f1da4bf48a89b901d055e3ba
choi <sjchoi0408@hanmail.net>
2018-09-06 12:00:16 +0900

    제네릭 적용

--- java110-project/src/main/java/bitcamp/java110/cms/util/ArrayList.java
+++ java110-project/src/main/java/bitcamp/java110/cms/util/ArrayList.java
@@ -1,12 +1,12 @@
 package bitcamp.java110.cms.util;
 
-public class ArrayList {
+public class ArrayList<T> {
     
     //개별적으로 관리해야 할 값이라면 인스턴스 변수를 사용하라!
-     Object[] list = new Object[5]; 
-     int index = 0;
+     private Object[] list = new Object[5]; 
+     private int index = 0;
     
-    public void add(Object obj) {    //모든 Object의 자손타입이 들어갈 수 있음
+    public void add(T obj) {    //모든 Object의 자손타입이 들어갈 수 있음
          
             if(index == list.length) {
                 increaseStorage();
@@ -41,11 +41,15 @@ public class ArrayList {
     public int size() {
         return this.index;//안썼어도 this는 생략된 것
     }
-    public Object get(int no) {
+    
+    
+    
+    @SuppressWarnings("unchecked")
+    public T get(int no) {
         if(no < 0 || no >= index) {
             return null;
         }
-            return list[no];
+            return (T)list[no];
     }
 
     
