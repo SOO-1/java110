f837ee7173136b5ea978387453494aa3283b4440
choi <sjchoi0408@hanmail.net>
2018-09-10 15:49:04 +0900

    학생관리에 DAO 적용

---
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/StudentDao.java
@@ -0,0 +1,46 @@
+package bitcamp.java110.cms.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bitcamp.java110.cms.domain.Student;
+
+public class StudentDao {
+    
+    private List<Student> list = new ArrayList<>();
+    
+    public int insert(Student student) {
+        for(Student item : list) {
+            if(item.getEmail().equals(student.getEmail())) {
+                return 0; //입력못하게
+            }
+        } //중복 검사
+        list.add(student);
+        return 1;
+    }
+    
+    public List<Student> findAll() {
+        return list;
+    }
+    
+    public Student findByEmail(String email) {
+        for(Student item : list) {
+            if(item.getEmail().equals(email)) {
+                return item; //입력못하게
+            }
+        } //중복 검사
+        return null;
+    }
+    
+    public int delete(String email) {
+        for(Student item : list) {
+            if(item.getEmail().equals(email)) {
+                list.remove(item); //제거
+                return 1;
+            }
+        } 
+        return 0;   //return이 0이면 실패, 1이면 성공
+    }
+    
+    
+}
