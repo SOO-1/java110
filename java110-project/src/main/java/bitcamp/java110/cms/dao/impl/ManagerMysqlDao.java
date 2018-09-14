efbc22af95c1b0634baaa13a25923e3651c8bdcc
choi <sjchoi0408@hanmail.net>
2018-09-14 10:57:23 +0900

    ManagerMysqlDao에서커넥션 속도비교

--- java110-project/src/main/java/bitcamp/java110/cms/dao/impl/ManagerMysqlDao.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/impl/ManagerMysqlDao.java
@@ -82,45 +82,14 @@ public class ManagerMysqlDao implements ManagerDao {
         ResultSet rs = null;    //결과를 가져오기 위해서.
         
         try {
-            // => java.sql.Driver 구현체를 로딩한다.
-            // => 해당 클래스의 객체를 만들어 DriverManager에 등록한다.
             Class.forName("org.mariadb.jdbc.Driver");
-
-            //DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
-            // => DriverManager에게 java.sql.Connection 객체를 요구한다.
-            // => DriverManager는 등록된 Driver들 중에서 요구 사항에 맞는
-            //    드라이버를 찾아 connect()를 호출한다.
-            //    그리고 connect() 메서드의 리턴값을 그대로 리턴해준다.
-
-            // 위의 한줄과 같은 기능. 객체 만들어서 등록해야함! 근데 Driver 클래스에 가보면 static으로 로딩되면서
-            // 자동으로 객체를 만들어주기 때문에 객체를 우리가 만들어 줄 필요가 없음. => 따라서 위의 한줄로 대체 가능!
 
             con = DriverManager.getConnection(
                     "jdbc:mariadb://localhost:3306/studydb"
                     , "study", "1111");
 
-            // => 질의문을 작성할 객체를 준비한다.
             stmt = con.createStatement();
 
-            // => select 질의를 한다. 
-            /*
-             * select 
-             *      m.name,
-             *      m.email,
-             *      mr.posi
-             * from p1_mgr mr inner join p1_memb m  // 여기서 inner는 넣어도, 안넣어도 inner join!
-             * on mr.mrno = m.mno;
-
-             * select 
-             *      m.name,
-             *      m.email,
-             *      mr.posi
-             * from p1_mgr mr right outer join p1_memb m  //오른쪽을 기준으로 오른쪽에 해당하는것 모두 출력
-             * on mr.mrno = m.mno;
-             *
-             *
-             */
-            //resultset은 서버에 가서 
             rs = stmt.executeQuery(
                     "  select " + 
                             "  m.mno," +
@@ -129,7 +98,6 @@ public class ManagerMysqlDao implements ManagerDao {
                             "  mr.posi" + 
                             "  from p1_mgr mr inner join p1_memb m" + 
                             "  on mr.mrno = m.mno");    //sql안에 ;넣지 않음.
-            // 서버에 생성된 질의 결과를 한개씩 가져온다.
             while(rs.next()) {
                 Manager mgr = new Manager();    //바깥으로 뽑으면 arraylist에 기존데이터가 날아가고 모든 것이 같은 주소를 가리키게 됨.
                 mgr.setNo(rs.getInt("mno"));
@@ -147,9 +115,59 @@ public class ManagerMysqlDao implements ManagerDao {
             try{ con.close(); } catch(Exception e) {}
         }
         return list;
-        //name과 mail,posi를 select했기 때문에 이외의 자리에는 null이 반환된다.
     }
 
+    Connection con;
+    {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost:3306/studydb"
+                    , "study", "1111");
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //인스턴스블럭 - 생성자가 호출되기 전에 호출된다.
+
+    public List<Manager> findAll2(){ 
+
+        ArrayList<Manager> list = new ArrayList<>();
+        Statement stmt = null;
+        ResultSet rs = null;    
+        
+        try {
+
+            stmt = con.createStatement();
+
+            rs = stmt.executeQuery(
+                    "  select " + 
+                            "  m.mno," +
+                            "  m.name," + 
+                            "  m.email," + 
+                            "  mr.posi" + 
+                            "  from p1_mgr mr inner join p1_memb m" + 
+                            "  on mr.mrno = m.mno");    
+            while(rs.next()) {
+                Manager mgr = new Manager();   
+                mgr.setNo(rs.getInt("mno"));
+                mgr.setEmail(rs.getString("email"));
+                mgr.setName(rs.getString("name"));
+                mgr.setPosition(rs.getString("posi"));
+
+                list.add(mgr);
+            }
+        }catch (Exception e) {
+            throw new DaoException(e);
+        }finally {
+            try{ rs.close(); } catch(Exception e) {}
+            try{ stmt.close(); } catch(Exception e) {}
+        }
+        return list;
+    }
+    
     public Manager findByEmail(String email) {
         
         Connection con = null;
@@ -192,7 +210,7 @@ public class ManagerMysqlDao implements ManagerDao {
         }finally {
             try{ rs.close(); } catch(Exception e) {}
             try{ stmt.close(); } catch(Exception e) {}
-            try{ con.close(); } catch(Exception e) {}
+            //끝났지만 인스턴스블록의 con을 닫지 않음.
         }
 
     }
@@ -282,7 +300,24 @@ public class ManagerMysqlDao implements ManagerDao {
     }
     
     
+    public static void main(String[] args) {
+        ManagerMysqlDao dao = new ManagerMysqlDao();
+        long start, end;
+        start = System.currentTimeMillis();    //1970년기준
+        for(int i=0; i<100; i++) {
+            dao.findAll();  //연결 - select - select 끊기
+        }
+        end = System.currentTimeMillis();
+        System.out.println(end-start);
+
+        start = System.currentTimeMillis();    //1970년기준
+        for(int i=0; i<100; i++) {
+            dao.findAll2(); //한번커넥션             => 매번 커넥션하는것보다 훨씬 빠름.
+        }
+        end = System.currentTimeMillis();
+        System.out.println(end-start);
     
+    }
     
     
     
