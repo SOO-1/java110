eddd05d622a5e789ac618536644cf75d2d538e49
choi <sjchoi0408@hanmail.net>
2018-10-19 11:31:39 +0900

    spring Java Config

--- java110-spring-ioc/src/main/java/ex08/MyBeanPostProcessor.java
+++ java110-spring-ioc/src/main/java/ex08/MyBeanPostProcessor.java
@@ -1,7 +1,11 @@
 package ex08;
 
+import java.lang.reflect.Method;
+
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
 
 public class MyBeanPostProcessor implements BeanPostProcessor{
 
@@ -10,6 +14,24 @@ public class MyBeanPostProcessor implements BeanPostProcessor{
                 Object bean, String beanName) throws BeansException {
         System.out.println("MyBeanPostProcessor.postProcessBeforeInitialization()");
         System.out.printf(" ==> %s\n",bean.toString());
+    
+/*        객체 정보가 들어오면 autowired 붙은 메서드 찾아 실행하도록 하는 beanPostProcessor와 같은 역할.
+        Class<?> clazz = bean.getClass();
+        Method[] methods = clazz.getMethods();
+        ApplicationContext iocContainer = null; 
+        for(Method m : methods) {
+            Autowired anno = m.getAnnotation(Autowired.class);
+            if(anno==null) continue;
+            
+            Class<?> paramType = m.getParameterTypes()[0];
+            Object paramObj = iocContainer.getBean(paramType);
+            
+            if(paramObj == null) continue;
+            try {
+                m.invoke(bean, paramType);
+            }catch(Exception e) {}
+        }
+*/        
         return bean;
     }
     
